060de63415873a4ed732a8b043afff6224935c95
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-03-27 14:38:20 -0700

    push for deployment for heroku app

--- server.js
+++ server.js
@@ -8,46 +8,58 @@ const store = require('./movie-store.json');
 
 
 const app = express();
-
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
-    const apiToken = process.env.API_TOKEN;
-    const authToken = req.get('Authorization');
-    if (!authToken || authToken.split(' ')[1] !== apiToken) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-    next();
-  });
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+  next();
+});
 
 
 
 app.get('/movie', (req, res) => {
-  const {genre, country, avg_vote} = req.query;
+  const { genre, country, avg_vote } = req.query;
 
   let response = store;
 
-    if (req.query.genre) {
-        response = response.filter(movie =>
-            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase()));
-    }
+  if (req.query.genre) {
+    response = response.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase()));
+  }
 
-    if (req.query.country) {
-        response = response.filter(movie =>
-            movie.country.toLowerCase().includes(req.query.country.toLowerCase()));
-    }
+  if (req.query.country) {
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase()));
+  }
 
-    if (req.query.avg_vote) {
-        response = response.filter(movie =>
-            Number(movie.avg_vote) >= Number(req.query.avg_vote));
-    }
+  if (req.query.avg_vote) {
+    response = response.filter(movie =>
+      Number(movie.avg_vote) >= Number(req.query.avg_vote));
+  }
 
 
   res.json(response);
 });
 
-  app.listen(8000, () => {
-    console.log('Express server is listening on port 8000!');
-  });
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
+  console.log(`Express server is listening http://localhost:${PORT}!`);
+});
